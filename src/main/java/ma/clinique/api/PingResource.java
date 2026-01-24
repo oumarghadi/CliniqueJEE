15b1c74725dcf49ee839cb120dfb3d317c2a74be
oumarghadi <oumarghadi95@gmail.com>
2026-01-24 18:48:55 +0100

    initialiser le point terminaison /api/ping

--- src/main/java/ma/clinique/api/PingResource.java
+++ src/main/java/ma/clinique/api/PingResource.java
@@ -1,5 +1,16 @@
 package ma.clinique.api;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/ping")
 public class PingResource {
 
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String ping() {
+    return "OK";
+  }
 }
